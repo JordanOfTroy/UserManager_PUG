cd3ed971e1b25baff3eb7d95eecb73a8045f01f3
Jordan <jordan@Jordans-MacBook-Air.local>
2023-05-11 10:19:31 -0600

    set up initial Server.js and initialized userList.json

---
+++ Server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const path = require('path')
+const {v4: uuidv4} = require('uuid')
+
+const server = express()
+const PORT = 3000
+
+server.use(express.urlencoded({extended: false}))
+
+server.set('views', path.join(__dirname, 'views'))
+server.set('view engine', 'pug')
+
+server.listen(PORT, () => {
+    console.log(`Master has given Dobby a port ${PORT}!\nDobby is free!!!`)
+})
