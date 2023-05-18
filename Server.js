e5dc341b865c8b9cf359dfca03b2eeb9d3f5dc8e
Jordan <jordan@Jordans-MacBook-Air.local>
2023-05-18 10:17:25 -0600

    displaying users in table

--- Server.js
+++ Server.js
@@ -4,7 +4,6 @@ const path = require("path");
 const { v4: uuidv4 } = require("uuid");
 const fs = require("fs");
 let data = require("./data/userList.json");
-// console.log(data.users[0].uuid)
 
 const server = express();
 const PORT = 3000;
@@ -36,9 +35,68 @@ server.post("/", (req, res) => {
       console.log("file updated");
     }
   );
-  res.render('userlist', {
-    users: data.users
+  res.render("userList", {
+    users,
   });
+});
+
+server.get("/users", (req, res) => {
+  let { users } = data;
+  res.render("userList", {
+    users: users,
+  });
+});
+
+server.get("/delete/:id", (req, res) => {
+  let { id } = req.params;
+  let { users } = data;
+  let ind = users.map((user) => user.uuid).indexOf(id);
+  users.splice(ind, 1);
+  let json = JSON.stringify(data);
+  fs.writeFile(
+    path.join(__dirname, "data", "userList.json"),
+    json,
+    "utf8",
+    () => {
+      console.log("file updated");
+      res.redirect("/users");
+    }
+  );
+  // res.redirect("/users");
+});
+
+
+server.get("/update/:id", (req, res) => {
+  let { id } = req.params;
+  let { users } = data;
+  let ind = users.map((user) => user.uuid).indexOf(id);
+  let userToUpdate = users[ind];
+  res.render("index", {
+    formTitle: `Updating ${userToUpdate.username}`,
+    formType: "create",
+    user: userToUpdate,
+  });
+});
+
+server.post("/update/:id", (req, res) => {
+  let { users } = data;
+  let { name, age, username, email } = req.body;
+  let { id } = req.params;
+  let ind = users.map((user) => user.uuid).indexOf(id);
+  currUser = { name, age, username, email, uuid: id };
+  data.users[ind] = currUser;
+  let json = JSON.stringify(data);
+
+  fs.writeFileSync(
+    path.join(__dirname, "data", "userList.json"),
+    json,
+    "utf8",
+    () => {
+      console.log("file updated");
+    }
+  );
+
+  res.redirect("/users");
 });
 
 server.listen(PORT, () => {
